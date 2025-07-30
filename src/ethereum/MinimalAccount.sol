1afa24b9b32f3ca5a466cddc032f795af14d8169
Sainath More <sainathmore501@gmail.com>
2025-07-30 13:02:50 +0000

    Working on ethereum in that MinimalAccount.sol file

---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MinimalAccount is IAccount {
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {}
+}
