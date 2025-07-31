ee62e1d6905ed36978661e676b7cde9e926fae1a
Sainath More <sainathmore501@gmail.com>
2025-07-31 04:50:04 +0000

    Work on MinimalAccount.sol file

---
+++ script/SendPackedUserOp.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
+
+contract DeployMinimal is Script {}
