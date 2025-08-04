31ed3a3e590d939a695d793d32b523745568c97b
Sainath More <sainathmore501@gmail.com>
2025-08-04 14:59:40 +0000

    work on ZkSync

---
+++ src/zksync/ZkMinimalAccount.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ZkMinimalAccount {}
