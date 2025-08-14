8d1b6ca43b1aeca2999e8a280fc648d264d3d7bf
Sainath More <sainathmore501@gmail.com>
2025-08-14 11:05:54 +0000

    working on ZkSync tests

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -4,4 +4,33 @@ pragma solidity ^0.8.20;
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ZkMinimalAccount {}
+contract ZkMinimalAccount {
+    /*//////////////////////////////////////////////////////////////
+                                  ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error ZkMinimalAccount__NotFromEntryPoint();
+    error ZkMinimalAccount__NotFromEntryPointOrOwner();
+    error ZkMinimalAccount__CallFailed(bytes);
+
+    /*//////////////////////////////////////////////////////////////
+                             STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    IAccount private immutable i_account;
+
+    /*//////////////////////////////////////////////////////////////
+                                MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+    modifier requireFromEntryPoint() {
+        if (msg.sender != address(i_account)) {
+            revert ZkMinimalAccount__NotFromEntryPoint();
+        }
+        _;
+    }
+
+    modifier requireFromEntryPointOrOwner() {
+        if (msg.sender != address(i_account) && msg.sender != owner()) {
+            revert ZkMinimalAccount__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+}
